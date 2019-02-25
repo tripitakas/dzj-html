48dce3ab2f5037022585a90d851b4d8aa50db78b
Zhang Yungui <rhcad@hotmail.com>
2019-02-25 20:15:01 +0800

    提供公共测试函数：加用户、登录

--- tests/task/test_text_task.py
+++ tests/task/test_text_task.py
@@ -5,39 +5,31 @@
 """
 from tests.testcase import APITestCase
 import controller.errors as e
+import model.user as u
 
-admin = 'admin@test.com', 'test123'
 user1 = 'text1@test.com', 't12345'
 user2 = 'text2@test.com', 't12312'
 
 
 class TestTextTask(APITestCase):
-    created = False
-
     def setUp(self):
         super(APITestCase, self).setUp()
-
-        self.add_admin_user()
-        self.fetch('/api/user/register', body={'data': dict(email=user1[0], name='文字测试', password=user1[1])})
-        self.fetch('/api/user/register', body={'data': dict(email=user2[0], name='测试文字', password=user2[1])})
-
-        self.fetch('/api/user/login', body={'data': dict(email=admin[0], password=admin[1])})
-        r1 = self.fetch('/api/user/change', body={'data': dict(email=user1[0], authority='文字校对员')})
-        r2 = self.fetch('/api/user/change', body={'data': dict(email=user2[0], authority='文字校对员')})
-        self.created = self.get_code(r1) in [200, e.no_change[0]] and self.get_code(r2) in [200, e.no_change[0]]
+        self.add_users([dict(email=user1[0], name='文字测试', password=user1[1]),
+                        dict(email=user2[0], name='测试文字', password=user2[1])], u.ACCESS_TEXT_PROOF)
 
     def tearDown(self):
+        
         # 退回所有任务
-        self.fetch('/api/user/login', body={'data': dict(email=admin[0], password=admin[1])})
-        r = self.fetch('/api/unlock/text1_proof/')
-        self.assert_code(200, r)
-
+        self.login_as_admin()
+        self.fetch('/api/unlock/text1_proof/')
+        
         super(APITestCase, self).setUp()
 
     def test_get_tasks_no_open(self):
         """ 测试默认未发布任务时文字校对任务取不到 """
-        r = self.fetch('/api/user/login', body={'data': dict(email=user1[0], password=user1[1])})
-        if self.get_code(r) == 200 and self.created:
+        
+        r = self.login(user1[0], user1[1])
+        if self.get_code(r) == 200:
             r = self.parse_response(self.fetch('/dzj_chars?_raw=1&count=1'))
             self.assertIn('tasks', r)
             self.assertEqual(len(r['tasks']), 0)
@@ -46,12 +38,12 @@ class TestTextTask(APITestCase):
         """ 测试文字校对任务的发布、列表和领取 """
 
         # 发布任务
-        self.add_admin_user()
+        self.login_as_admin()
         r = self.fetch('/api/start/', body=dict(data=dict(types='text1_proof')))
         self.assertIn('names', self.parse_response(r))
 
-        r = self.fetch('/api/user/login', body={'data': dict(email=user1[0], password=user1[1])})
-        if self.get_code(r) == 200 and self.created:
+        r = self.login(user1[0], user1[1])
+        if self.get_code(r) == 200:
             # 取任务列表
             r = self.parse_response(self.fetch('/dzj_chars?_raw=1&count=1'))
             self.assertIn('tasks', r)
@@ -68,11 +60,14 @@ class TestTextTask(APITestCase):
             self.assertEqual('待继续', r['tasks'][0].get('status'))
             self.assertNotEqual(r['tasks'][-1]['name'], name)
 
+            # 可以继续上次未完成的任务
+            self.assertIn('name', self.parse_response(self.fetch('/api/pick/text1_proof/' + name)))
+
             # 未完成时不能领取新的任务
             r = self.fetch('/api/pick/text1_proof/' + r['tasks'][-1].get('name'))
             self.assert_code(e.task_uncompleted, r)
 
-            self.fetch('/api/user/login', body={'data': dict(email=user2[0], password=user2[1])})
+            self.login(user2[0], user2[1])
 
             # 其他人不能领取相同任务
             r = self.fetch('/api/pick/text1_proof/' + name)
--- tests/testcase.py
+++ tests/testcase.py
@@ -86,3 +86,17 @@ class APITestCase(AsyncHTTPSTestCase):
         """ 在创建其他用户前先创建超级管理员，避免测试用例乱序执行时其他用户先创建而成为管理员 """
         return self.fetch('/api/user/register', body={'data': dict(
             email='admin@test.com', name='管理', password='test123')})
+
+    def add_users(self, users, auth=None):
+        self.add_admin_user()
+        for r in users:
+            self.fetch('/api/user/register', body={'data': r})
+        self.add_admin_user()
+        for r in users:
+            self.fetch('/api/user/change', body={'data': dict(email=r['email'], authority=r.get('authority', auth))})
+
+    def login_as_admin(self):
+        return self.login('admin@test.com', 'test123')
+
+    def login(self, email, password):
+        return self.fetch('/api/user/login', body={'data': dict(email=email, password=password)})
