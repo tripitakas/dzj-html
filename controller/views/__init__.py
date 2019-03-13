88ba2fe3ce1a8e8bf63e3f3c773fe11857bbcce9
xiandu <lqs.xiandu@qq.com>
2019-03-13 17:16:02 +0800

    如是藏经、实体藏经、藏经数据管理的链接修改

--- controller/views/__init__.py
+++ controller/views/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 在 controller.views 包实现页面响应类，生成前端页面，modules 为重用网页片段的渲染类
 
-from . import modules, home, user, task
+from . import modules, home, user, task, tripitaka as t
 
 handlers = [home.HomeHandler,
             user.UserLoginHandler, user.UserRegisterHandler,
@@ -11,7 +11,9 @@ handlers = [home.HomeHandler,
             task.ChooseCutProofHandler, task.ChooseCutReviewHandler,
             task.CharProofDetailHandler, task.CutProofDetailHandler,
             task.CutStatusHandler, task.TextStatusHandler,
-            task.TaskLobbyHandler, task.TaskAdminHandler, task.TaskCutStatusHandler, task.TaskTextStatusHandler, ]
+            task.TaskLobbyHandler, task.TaskAdminHandler, task.TaskCutStatusHandler, task.TaskTextStatusHandler,
+            t.RsTripitakaHandler, t.TripitakaListHandler, t.TripitakaHandler, t.DataTripitakaHandler,
+            t.DataEnvelopHandler, t.DataVolumeHandler, t.DataSutraHandler, t.DataReelHandler, t.DataPageHandler,]
 
 modules = {'CommonLeft': modules.CommonLeft, 'CommonHead': modules.CommonHead,
            'Pager': modules.Pager}
---
+++ controller/views/tripitaka.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@desc: 如是藏经、实体藏经、藏经数据管理
+@time: 2019/3/13
+"""
+
+from tornado.web import authenticated
+from controller.handler.base import BaseHandler
+from controller.helper import fetch_authority
+
+
+class RsTripitakaHandler(BaseHandler):
+    URL = '/tripitaka/rs'
+
+    def get(self):
+        """ 如是藏经 """
+        self.render('tripitaka_rs.html')
+
+
+class TripitakaListHandler(BaseHandler):
+    URL = '/tripitaka'
+
+    def get(self):
+        """ 藏经列表 """
+        self.render('tripitaka_list.html')
+
+class TripitakaHandler(BaseHandler):
+    URL = '/tripitaka/@tripitaka_id'
+
+    def get(self):
+        """ 单个实体藏经 """
+        self.render('tripitaka.html')
+
+
+
+class DataTripitakaHandler(BaseHandler):
+    URL = '/data/tripitaka'
+
+    def get(self):
+        """ 数据管理-实体藏 """
+        self.render('data_tripitaka.html')
+
+
+class DataEnvelopHandler(BaseHandler):
+    URL = '/data/envelop'
+
+    def get(self):
+        """ 数据管理-实体函 """
+        self.render('data_envelop.html')
+
+class DataVolumeHandler(BaseHandler):
+    URL = '/data/volume'
+
+    def get(self):
+        """ 数据管理-实体册 """
+        self.render('data_volume.html')
+
+
+class DataSutraHandler(BaseHandler):
+    URL = '/data/sutra'
+
+    def get(self):
+        """ 数据管理-实体经 """
+        self.render('data_sutra.html')
+
+
+class DataReelHandler(BaseHandler):
+    URL = '/data/reel'
+
+    def get(self):
+        """ 数据管理-实体卷 """
+        self.render('data_reel.html')
+
+class DataPageHandler(BaseHandler):
+    URL = '/data/page'
+
+    def get(self):
+        """ 数据管理-实体页 """
+        self.render('data_page.html')
