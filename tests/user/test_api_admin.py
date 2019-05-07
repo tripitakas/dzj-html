9d40347452c4003a0f46a017acb40544cf3139d6
xiandu <lqs.xiandu@qq.com>
2019-05-07 20:36:40 +0800

    完善user模块test_api_admin

--- tests/user/test_api_admin.py
+++ tests/user/test_api_admin.py
@@ -11,6 +11,7 @@ import tests.users as u
 class TestUserAdminApi(APITestCase):
     def setUp(self):
         super(TestUserAdminApi, self).setUp()
+        self.add_first_user_as_admin_then_login()
 
     def _get_user_by_email(self, email):
         return self._app.db.user.find_one(dict(email=email))
@@ -21,9 +22,8 @@ class TestUserAdminApi(APITestCase):
 
     def test_api_admin_roles(self):
         """ 给用户授予角色 """
-        self.add_first_user_as_admin_then_login()
-        self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
-        uid = self._get_id_by_email(u.user1[0])
+        users = self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
+        uid = users[0]['_id']
         r = self.fetch('/api/user/role', body={'data': dict(_id=uid, email=u.user1[0], roles='切分校对员')})
         self.assert_code(200, r)
         data = self.parse_response(r)
@@ -31,9 +31,8 @@ class TestUserAdminApi(APITestCase):
 
     def test_api_admin_reset_password(self):
         """ 重置用户密码 """
-        self.add_first_user_as_admin_then_login()
-        self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
-        uid = self._get_id_by_email(u.user1[0])
+        users = self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
+        uid = users[0]['_id']
         r = self.fetch('/api/user/reset_pwd', body={'data': dict(_id=uid)})
         self.assert_code(200, r)
         data = self.parse_response(r)
@@ -41,34 +40,31 @@ class TestUserAdminApi(APITestCase):
 
     def test_api_admin_change_profile(self):
         """ 修改用户profile """
-        self.add_first_user_as_admin_then_login()
-        self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
-        user1 = self._get_user_by_email(u.user1[0])
-        self.assertIsNotNone(user1)
+        users = self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
+        user1 = users[0]
         self.add_users_by_admin([dict(email=u.user2[0], password=u.user2[1], name=u.user2[2])])
 
         # 邮箱不能重复
-        body = {'data': dict(_id=user1['_id'], name=user1['name'], email=u.user2[0], phone=user1['phone'])}
+        body = {'data': dict(_id=user1['_id'], name=user1['name'], email=u.user2[0], phone=user1.get('phone'))}
         r = self.fetch('/api/user/profile', body=body)
         self.assert_code(e.record_existed, r)
 
         # 邮箱格式有误
         r = self.fetch('/api/user/profile', body={
-            'data': dict(_id=user1['_id'], name=user1['name'], email='123#123', phone=user1['phone'])
+            'data': dict(_id=user1['_id'], name=user1['name'], email='123#123', phone=user1.get('phone'))
         })
         self.assert_code(e.invalid_email, r)
 
         # 正常修改
         r = self.fetch('/api/user/profile', body={
-            'data': dict(_id=user1['_id'], name=user1['name'], email='user1_new@test.com', phone=user1['phone'])
+            'data': dict(_id=user1['_id'], name=user1['name'], email='user1_new@test.com', phone=user1.get('phone'))
         })
         self.assert_code(200, r)
 
-
     def test_api_delete_user(self):
         """ 测试删除用户 """
-        self.add_first_user_as_admin_then_login()
-        self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
-        uid = self._get_id_by_email(u.user1[0])
+        users = self.add_users_by_admin([dict(email=u.user1[0], password=u.user1[1], name=u.user1[2])])
+        uid = users[0]['_id']
         r = self.fetch('/api/user/delete', body={'data': dict(_id=uid)})
         self.assert_code(200, r)
+
