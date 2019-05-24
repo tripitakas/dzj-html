d800c46f58e767194ef5796daa65987641dac8c0
xiandu <lqs.xiandu@qq.com>
2019-05-24 14:35:05 +0800

    完善base代码

--- controller/base.py
+++ controller/base.py
@@ -78,10 +78,8 @@ class BaseHandler(CorsMixin, RequestHandler):
 
     def can_access(self, path, method='GET'):
         """检查当前用户是否能访问某个(path, method)"""
-        if not self.current_user:
-            return can_access('访客', path, method)
-        role = self.current_user.get('roles')
-        role = role if role else '普通用户'
+        role = '访客' if not self.current_user else '普通用户' if not self.current_user.get('roles') else self.current_user[
+            'roles']
         return can_access(role, path, method)
 
     def get_current_user(self):
