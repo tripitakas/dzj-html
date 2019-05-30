7a58fcb594ee9aa65e92d0176261a232796270a7
xiandu <lqs.xiandu@qq.com>
2019-05-30 22:23:45 +0800

    完善pager

--- controller/com/modules.py
+++ controller/com/modules.py
@@ -98,13 +98,10 @@ class Pager(UIModule):
             pager['page_count'] = math.ceil(pager['item_count'] / pager['page_size'])  # 一共有多少页
             pager['display_count'] = conf['display_count']  # pager导航条中显示多少个页码
             pager['path'] = self.request.path  # 当前path
-
-            gap, if_left, cur_page = int(pager['display_count'] / 2), int(pager['display_count']) % 2, pager['cur_page']
-            start, end = cur_page - gap, cur_page + gap - 1 + if_left
-            offset = 1 - start if start < 1 else pager['page_count'] - end if pager['page_count'] < end else 0
-            start, end = start + offset, end + offset
+            start = pager['cur_page'] - int(pager['display_count'] / 2)
             start = 1 if start < 1 else start
-            end = pager['page_count'] if end > pager['page_count'] else end
+            end = start + pager['display_count']
+            end = pager['page_count'] if pager['page_count'] < end else end
             pager['display_range'] = range(start, end + 1)
 
         return self.render_string('_pager.html', pager=pager)
