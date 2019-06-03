ff8f4dbabc237f241947a691ad77989a6690dea8
Zhang Yungui <rhcad@hotmail.com>
2019-06-03 23:08:49 +0800

    准备实现根据坐标重新生成栏、列、字框的编号的接口，用于字序校对分配新列号

--- controller/data/__init__.py
+++ controller/data/__init__.py
@@ -1,8 +1,10 @@
 from . import data as t
+from . import api_algorithm as g
 
 views = [
     t.DataTripitakaHandler,
     t.DataEnvelopHandler, t.DataVolumeHandler, t.DataSutraHandler, t.DataReelHandler, t.DataPageHandler,
 ]
 handlers = [
+    g.GenerateCharIdApi,
 ]
