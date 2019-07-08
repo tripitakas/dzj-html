5ea1b3d49fdc6a06aa29ab99b48d43f8741a03ae
xiandu <lqs.xiandu@qq.com>
2019-07-08 19:38:55 +0800

    更新add_pages

--- tests/add_pages.py
+++ tests/add_pages.py
@@ -4,21 +4,30 @@
 # 本脚本的执行结果相当于在“数据管理-实体页”中提供了图片、OCR切分数据、文本，是任务管理中发布切分和文字审校任务的前置条件。
 # python tests/add_pages.py --json_path=切分文件路径 [--img_path=页面图路径] [--txt_path=经文路径] [--kind=藏经类别码]
 
-from tornado.util import PY3
-from os import path, listdir, mkdir
 import sys
 import os
 import json
 import re
 import shutil
 import pymongo
+from tornado.util import PY3
 from datetime import datetime
+from os import path, listdir, mkdir
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from controller.task.base import TaskHandler as task
 
 IMG_PATH = path.join(path.dirname(__file__), '..', 'static', 'img')
 data = dict(count=0)
+
+base_fields = [
+    'name', 'kind', 'width', 'height', 'blocks', 'columns', 'chars', 'ocr', 'cmp1', 'txt1_html', 'cmp2', 'txt2_html',
+    'cmp3', 'txt3_html', 'txt_review_html', 'txt_hard_html', 'text', 'lock', 'tasks', 'create_time'
+]
+task_types = [
+    'block_cut_proof', 'block_cut_review', 'column_cut_proof', 'column_cut_review', 'char_cut_proof',
+    'char_cut_review', 'text_proof_1', 'text_proof_2', 'text_proof_3', 'text_review', 'text_hard'
+]
 
 
 def create_folder(filename):
@@ -77,7 +86,8 @@ def add_repeat_pages(name, info, db, repeat, use_local_img=False):
 def _add_repeat_pages(name, info, db, start, end, use_local_img):
     meta_list = []
     for i in range(start, end + 1):
-        meta = dict(
+        meta = {k: None for k in base_fields}
+        meta.update(dict(
             name='%s_%s' % (name, i),
             img_name=name,
             kind=name[:2],
@@ -86,27 +96,16 @@ def _add_repeat_pages(name, info, db, start, end, use_local_img):
             blocks=info.get('blocks', []),
             columns=info.get('columns', []),
             chars=info.get('chars', []),
-            text=None,
-            ocr=None,
-            tasks=None,
             create_time=datetime.now()
-        )
+        ))
         if use_local_img:
             meta['use_local_img'] = True
-        # initialize task
-        meta.update(dict(tasks={
-            'block_cut_proof': {'status': task.STATUS_READY},
-            'block_cut_review': {'status': task.STATUS_READY},
-            'column_cut_proof': {'status': task.STATUS_READY},
-            'column_cut_review': {'status': task.STATUS_READY},
-            'char_cut_proof': {'status': task.STATUS_READY},
-            'char_cut_review': {'status': task.STATUS_READY},
-        }))
+        # initialize tasks
+        meta.update(dict(tasks={t: dict(status=task.STATUS_READY) for t in task_types}))
         meta_list.append(meta)
     db.page.insert_many(meta_list)
 
     data['count'] += end - start + 1
-
     print('%s[%d:%d]:\t\t%d x %d blocks=%d columns=%d chars=%d' % (
         name, start, end, meta['width'], meta['height'], len(meta['blocks']), len(meta['columns']),
         len(meta['chars']),
@@ -115,7 +114,8 @@ def _add_repeat_pages(name, info, db, start, end, use_local_img):
 
 def add_page(name, info, db, img_name=None, use_local_img=False):
     if not db.page.find_one(dict(name=name)):
-        meta = dict(
+        meta = {k: None for k in base_fields}
+        meta.update(dict(
             name=name,
             kind=name[:2],
             width=int(info['imgsize']['width']),
@@ -123,32 +123,14 @@ def add_page(name, info, db, img_name=None, use_local_img=False):
             blocks=info.get('blocks', []),
             columns=info.get('columns', []),
             chars=info.get('chars', []),
-            ocr='',
-            cmp1='',
-            txt1_html='',
-            cmp2='',
-            txt2_html='',
-            cmp3='',
-            txt3_html='',
-            txt_review_html='',
-            text='',
-            lock={},
-            tasks={},
             create_time=datetime.now()
-        )
+        ))
         if img_name:
             meta['img_name'] = img_name
         if use_local_img:
             meta['use_local_img'] = True
-        # initialize task
-        meta.update(dict(tasks={
-            'block_cut_proof': {'status': task.STATUS_READY},
-            'block_cut_review': {'status': task.STATUS_READY},
-            'column_cut_proof': {'status': task.STATUS_READY},
-            'column_cut_review': {'status': task.STATUS_READY},
-            'char_cut_proof': {'status': task.STATUS_READY},
-            'char_cut_review': {'status': task.STATUS_READY},
-        }))
+        # initialize tasks
+        meta.update(dict(tasks={t: dict(status=task.STATUS_READY) for t in task_types}))
         data['count'] += 1
         print('%s:\t%d x %d blocks=%d colums=%d chars=%d' % (
             name, meta['width'], meta['height'], len(meta['blocks']), len(meta['columns']), len(meta['chars'])))
@@ -168,14 +150,7 @@ def add_texts(src_path, pages, db):
             cond = {'$or': [dict(name=fn[:-4]), dict(img_name=fn[:-4])]}
             r = list(db.page.find(cond))
             if r and not r[0].get('text'):
-                meta = {
-                    'ocr': text,
-                    'tasks.text_proof_1': {'status': task.STATUS_READY},
-                    'tasks.text_proof_2': {'status': task.STATUS_READY},
-                    'tasks.text_proof_3': {'status': task.STATUS_READY},
-                    'tasks.text_review': {'status': task.STATUS_READY}
-                }
-                db.page.update_many(cond, {'$set': meta})
+                db.page.update_many(cond, {'$set': dict(ocr=text)})
 
 
 def copy_img_files(src_path, pages):
