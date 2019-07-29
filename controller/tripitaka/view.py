0125c270b332fbf95fbde52822f2bd31ffe87a67
xiandu <lqs.xiandu@qq.com>
2019-07-29 22:35:51 +0800

    删除CBETA阅读和搜索功能

--- controller/tripitaka/view.py
+++ controller/tripitaka/view.py
@@ -4,8 +4,6 @@
 @desc: 如是藏经、实体藏经
 @time: 2019/3/13
 """
-import os
-import lxml.etree as etree
 from controller.base import BaseHandler
 
 
@@ -15,21 +13,6 @@ class RsTripitakaHandler(BaseHandler):
     def get(self):
         """ 如是藏经 """
         self.render('tripitaka_rs.html')
-
-
-class CbetaHandler(BaseHandler):
-    URL = '/cbeta'
-
-    def get(self):
-        """ CBETA """
-        xsl = open('%s/taisho.xsl' % os.path.dirname(os.path.realpath(__file__)), 'rb')
-        xslt = etree.XML(xsl.read())
-        transform = etree.XSLT(xslt)
-        xml = etree.parse(os.path.join(self.application.BASE_DIR, 'static/xml/T/T10/T10n0279_001._xml'))
-        content = transform(xml)
-        article = str(content)
-        article = article[article.find('<body>') + 6: article.rfind('</body>')]
-        self.render('tripitaka_cbeta.html', article=article)
 
 
 class TripitakaListHandler(BaseHandler):
