21b576174f5052091fb7ab049e8327f8bebb95cc
Zhang Yungui <rhcad@hotmail.com>
2019-08-07 15:25:04 +0800

    加导入过程提示

--- meta/import_meta.py
+++ meta/import_meta.py
@@ -25,12 +25,15 @@ def get_date_time(fmt=None, diff_seconds=None):
 def import_tripitaka():
     """ 导入tripitaka meta数据 """
     meta_csv = path.join(META_DIR, 'Tripitaka.csv')
-    sys.stdout.write('import tripitaka: %s...' % path.basename(meta_csv))
+    sys.stdout.write('import tripitaka: %s ' % path.basename(meta_csv))
     with open(meta_csv) as fn:
         rows = list(csv.reader(fn))
         heads = rows[0]
         added = 0
-        for row in rows[1:]:
+        for r, row in enumerate(rows[1:]):
+            if r % 50 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
             data = {heads[i]: item for i, item in enumerate(row)}
             data.update({
                 'create_time': get_date_time(),
@@ -52,12 +55,15 @@ def get_code_value(code):
 
 def import_volume(tripitaka, meta_csv):
     """ 导入volume meta数据 """
-    sys.stdout.write('import volume: %s...' % path.basename(meta_csv))
+    sys.stdout.write('import volume: %s ' % path.basename(meta_csv))
     with open(meta_csv) as fn:
         rows = list(csv.reader(fn))
         heads = rows[0]
         added = 0
-        for row in rows[1:]:
+        for r, row in enumerate(rows[1:]):
+            if r % 50 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
             data = {heads[i]: item for i, item in enumerate(row)}
             content_pages = json.loads(data['content_pages'].replace("'", '"'))
             content_pages = [p for p in content_pages if '_f' not in p and '_b' not in p]
@@ -84,12 +90,15 @@ def import_volume(tripitaka, meta_csv):
 
 def import_sutra(tripitaka, meta_csv):
     """ 导入volume meta数据 """
-    sys.stdout.write('import sutra: %s...' % path.basename(meta_csv))
+    sys.stdout.write('import sutra: %s ' % path.basename(meta_csv))
     with open(meta_csv) as fn:
         rows = list(csv.reader(fn))
         heads = rows[0]
         added = 0
-        for row in rows[1:]:
+        for r, row in enumerate(rows[1:]):
+            if r % 50 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
             data = {heads[i]: item for i, item in enumerate(row)}
             data.update({
                 'create_time': get_date_time(),
@@ -103,12 +112,15 @@ def import_sutra(tripitaka, meta_csv):
 
 def import_reel(tripitaka, meta_csv):
     """ 导入volume meta数据 """
-    sys.stdout.write('import reel: %s...' % path.basename(meta_csv))
+    sys.stdout.write('import reel: %s ' % path.basename(meta_csv))
     with open(meta_csv) as fn:
         rows = list(csv.reader(fn))
         heads = rows[0]
         added = 0
-        for row in rows[1:]:
+        for r, row in enumerate(rows[1:]):
+            if r % 50 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
             data = {heads[i]: item for i, item in enumerate(row)}
             data.update({
                 'create_time': get_date_time(),
