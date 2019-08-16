2ff10ed69eb8d8464145003e549e74aad27f812c
xiandu <lqs.xiandu@qq.com>
2019-08-16 09:13:15 +0800

    更新meta下的js

--- meta/build_js.py
+++ meta/build_js.py
@@ -29,7 +29,7 @@ def build_volume_js(db):
     js_dir = path.join(base_dir, 'static', 'js', 'meta')
     tripitakas = db.tripitaka.find({'img_available': '是'})
     for t in tripitakas:
-        print('generating %s-volume.js ...' % t)
+        print('generating %s-volume.js ...' % t.get('tripitaka_code'))
         volumes = db.volume.find({'tripitaka_code': t['tripitaka_code']}).sort([('envelop_no', 1), ('volume_no', 1)])
         volume_tree = get_volume_tree(volumes, t['store_pattern'])
         js_file = path.join(js_dir, '%s-volume.js' % t['tripitaka_code'])
@@ -59,9 +59,9 @@ def build_sutra_js(db):
         rows = list(db.sutra.find({'sutra_code': {'$regex': '^%s.*' % t}}, fields))
         rows.sort(key=cmp_to_key(lambda a, b: int(a['sutra_code'][3:]) - int(b['sutra_code'][3:])))
         rows = [[
-            r.get('sutra_code', ''), r.get('sutra_name', ''), r.get('due_reel_count', ''),
-            r.get('existed_reel_count', ''), r.get('start_volume', ''), r.get('start_page', ''),
-            r.get('end_volume', ''), r.get('end_page', ''),
+            r.get('sutra_code') or '', r.get('sutra_name') or '', r.get('due_reel_count') or '',
+            r.get('existed_reel_count') or '', r.get('start_volume') or '', r.get('start_page') or '',
+            r.get('end_volume') or '', r.get('end_page') or '',
         ] for r in rows]
         js_file = path.join(js_dir, '%s-sutra.js' % t)
         with open(js_file, 'w', encoding='utf-8') as fp:
@@ -76,7 +76,7 @@ def build_sutra_js(db):
             fp.write(";")
 
 
-def main(db_name='tripitaka', uri='localhost', which='sutra'):
+def main(db_name='tripitaka', uri='localhost', which='sutra, volume'):
     conn = pymongo.MongoClient(uri)
     db = conn[db_name]
     if 'volume' in which:
