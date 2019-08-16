ad3cfc85c86d6d2eb1d0e099a22acf70d229a5ca
Zhang Yungui <rhcad@hotmail.com>
2019-08-16 10:34:27 +0800

    强制非debug模式

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ define('num_processes', default=4, help='sub-processes count', type=int)
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     opt.parse_command_line()
-    opt.debug = opt.debug and opt.port not in [80, 443] and os.name != 'linux'
+    opt.debug = False
     routes = c.handlers + c.views
     app = Application(routes, default_handler_class=c.InvalidPageHandler, ui_modules=c.modules, xsrf_cookies=True)
     try:
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         fork_id = 0 if opt.debug or os.name == 'nt' else process.fork_processes(opt.num_processes)
         server.add_sockets(sockets)
         protocol = 'https' if ssl_options else 'http'
-        logging.info('Start the service #%d v%s on %s %s://localhost:%d' % (fork_id, app.version, os.name, protocol, opt.port))
+        logging.info('Start the service #%d v%s on %s://localhost:%d' % (fork_id, app.version, protocol, opt.port))
         if fork_id == 0:
             ioloop.PeriodicCallback(partial(periodic_task, app), 1000 * 300).start()
         ioloop.IOLoop.current().start()
