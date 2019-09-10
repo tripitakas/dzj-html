b5a8c51e32fc7605aacb2664a8367681d38829df
Zhang Yungui <rhcad@hotmail.com>
2019-09-10 11:44:48 +0800

    实现OCR展示

--- controller/data/ocr_api.py
+++ controller/data/ocr_api.py
@@ -9,6 +9,7 @@ from tornado.escape import to_basestring
 from urllib.parse import urlencode
 from controller.base import BaseHandler
 from controller import errors
+from controller.layout.v2 import calc
 from os import path
 import logging
 import re
@@ -28,7 +29,7 @@ class RecognitionApi(BaseHandler):
             with open(img_file.split('.')[0] + '.json', 'w') as f:
                 json.dump(r, f, ensure_ascii=False)
 
-            self.render('data_ocr.html', page=r, img=self.static_url('upload/ocr/' + filename))
+            self.render('data_ocr.html', page=self.ocr2page(r), img=self.static_url('upload/ocr/' + filename))
 
         data = self.get_request_data()
         if not data:
@@ -42,6 +43,37 @@ class RecognitionApi(BaseHandler):
             filename = '%d.%s' % (hash(img[0]['filename']) % 10000, filename.split('.')[-1])
 
         logging.info('recognize ' + filename)
-        self.call_back_api('http://127.0.0.1:8010/ocr?%s' % urlencode(data), timeout=15,
+        self.call_back_api('http://127.0.0.1:8010/ocr?%s' % urlencode(data), connect_timeout=10,
                            handle_error=lambda t: self.send_error_response(errors.ocr, message=errors.ocr[1] % t),
                            body=img[0]['body'], method='POST', handle_response=handle_response)
+
+    @staticmethod
+    def ocr2page(page):
+        def union(r1, r2):
+            if not r1:
+                r1 = list(r2)
+            else:
+                r1[0] = min(r1[0], r2[0])  # x1
+                r1[1] = min(r1[1], r2[1])  # y1
+                r1[2] = max(r1[2], r2[2])  # x2
+                r1[3] = max(r1[3], r2[3])  # y2
+            return r1
+
+        def union_list(items):
+            ret = None
+            for r in items:
+                ret = union(ret, r)
+            return dict(x=ret[0], y=ret[1], w=ret[2] - ret[0], h=ret[3] - ret[1])
+
+        page['blocks'] = [union_list(page['chars_pos'])]
+        page['columns'] = []
+        page['chars'] = [dict(x=c[0], y=c[1], w=c[2] - c[0], h=c[3] - c[1],
+                              cc=page['chars_cc'][i], txt=page['chars_text'][i])
+                         for i, c in enumerate(page['chars_pos'])]
+        chars = calc(page['chars'], page['blocks'], page['columns'])
+        for c_i, c in enumerate(chars):
+            page['chars'][c_i]['char_id'] = 'b%dc%dc%d' % (c['block_id'], c['column_id'], c['column_order'])
+            page['chars'][c_i]['block_no'] = c['block_id']
+            page['chars'][c_i]['line_no'] = c['column_id']
+            page['chars'][c_i]['char_no'] = chars[c_i]['no'] = c['column_order']
+        return page
--- controller/data/ocr_view.py
+++ controller/data/ocr_view.py
@@ -5,6 +5,7 @@
 @time: 2019/9/2
 """
 from controller.base import BaseHandler
+from controller.data.ocr_api import RecognitionApi
 from os import path
 import json
 
@@ -15,9 +16,9 @@ class RecognitionHandler(BaseHandler):
     def get(self):
         """ 藏经OCR页面 """
         try:
-            json_file = path.join(self.application.BASE_DIR, 'static', 'imgs', '001_006_A.json')
+            json_file = path.join(self.application.BASE_DIR, 'static', 'imgs', '1_6A.json')
             page = json.load(open(json_file))
-            self.render('data_ocr.html', page=page, img=self.static_url('imgs/001_006_A.gif'))
+            self.render('data_ocr.html', page=RecognitionApi.ocr2page(page), img=self.static_url('imgs/1_6A.gif'))
 
         except Exception as e:
             return self.send_db_error(e, render=True)
