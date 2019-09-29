1124619828a2a686a7c5ff2c096f6b55d1b9a2ce
xiandu <lqs.xiandu@qq.com>
2019-09-29 09:37:22 +0800

    清理代码

--- tests/update_pages.py
+++ tests/update_pages.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import os
 import pymongo
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 
 def main(db_name='tripitaka', uri='localhost'):
