3fe524da34a32b4cc0ed943be5d79c8b78f0fab0
Zhang Yungui <rhcad@hotmail.com>
2019-10-11 16:22:25 +0800

    update_pages.py 增加reset参数

--- tests/update_pages.py
+++ tests/update_pages.py
@@ -4,11 +4,12 @@
 import pymongo
 
 
-def main(db_name='tripitaka', uri='localhost'):
+def main(db_name='tripitaka', uri='localhost', reset=False):
     """
     重置page表
     :param db_name: 数据库名
     :param uri: 数据库服务器的地址，可为localhost或mongodb://user:password@server
+    :param reset: 是重置全部状态还是将旧版本数据库里缺失的任务字段补上
     """
     conn = pymongo.MongoClient(uri)
     db = conn[db_name]
@@ -25,9 +26,15 @@ def main(db_name='tripitaka', uri='localhost'):
         },
         'lock': {}
     }
-    fields = ['cmp1', 'txt1_html', 'cmp2', 'txt2_html', 'cmp3', 'txt3_html']
-    r = db.page.update_many({}, {'$set': update, '$unset': {k: '' for k in fields}})
-    print('%s records updated!' % r.modified_count)
+    if reset:
+        fields = ['cmp1', 'txt1_html', 'cmp2', 'txt2_html', 'cmp3', 'txt3_html']
+        r = db.page.update_many({}, {'$set': update, '$unset': {k: '' for k in fields}})
+        print('%s records updated!' % r.modified_count)
+    else:
+        for t in update['tasks'].keys():
+            r = db.page.update_many({'tasks.%s.status' % t: None}, {'$set': {'tasks.%s.status' % t: 'ready'}})
+            if r.modified_count:
+                print('%s %s records updated!' % (r.modified_count, t))
 
 
 if __name__ == '__main__':
