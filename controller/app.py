f4cafe4d1534eaac16c3f853e8e85a06baa2f90d
Zhang Yungui <rhcad@hotmail.com>
2019-10-17 17:36:52 +0800

    准备在SM上测试OCR流程

--- controller/app.py
+++ controller/app.py
@@ -18,7 +18,7 @@ from tornado.util import PY3
 from tornado.log import access_log
 from controller.role import url_placeholder
 
-__version__ = '0.0.54.90925'
+__version__ = '0.0.55.91017'
 BASE_DIR = path.dirname(path.dirname(__file__))
 
 define('testing', default=False, help='the testing mode', type=bool)
