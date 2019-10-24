0b0e8709dafd96a735f70dc1740e731973d5f3cb
xiandu <lqs.xiandu@qq.com>
2019-10-24 23:07:38 +0800

    fix testcase

--- tests/task/test_data_lock.py
+++ tests/task/test_data_lock.py
@@ -15,7 +15,7 @@ class TestDataLock(APITestCase):
         self.add_first_user_as_admin_then_login()
         self.add_users_by_admin(
             [dict(email=r[0], name=r[2], password=r[1]) for r in [u.expert1, u.expert2, u.expert3]],
-            '切分专家,文字专家,单元测试用户'
+            '切分专家,文字专家,数据处理员,单元测试用户'
         )
         self.delete_tasks_and_locks()
 
