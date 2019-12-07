4e2c1bebf1383bd60d9b898987d1bad85ecfec94
Zhang Yungui <rhcad@hotmail.com>
2019-12-07 21:59:46 +0800

    修正删除字框后不能保存的问题

--- controller/app.py
+++ controller/app.py
@@ -18,7 +18,7 @@ from tornado.util import PY3
 from tornado.log import access_log
 from controller.auth import url_placeholder
 
-__version__ = '0.0.66.91208'
+__version__ = '0.0.67.91207'
 BASE_DIR = path.dirname(path.dirname(__file__))
 
 define('testing', default=False, help='the testing mode', type=bool)
