3d44d10146958c4c580aab9ac905dfafaf87a589
xiandu <lqs.xiandu@qq.com>
2019-12-21 10:37:07 +0800

    fix testcase

--- tests/task/test_task_lock.py
+++ tests/task/test_task_lock.py
@@ -16,12 +16,8 @@ class TestDataLock(APITestCase):
         # 创建几个专家用户（权限足够），用于审校流程的测试
         self.add_first_user_as_admin_then_login()
         self.add_users_by_admin(
-            [dict(email=r[0], name=r[2], password=r[1]) for r in [u.expert1]],
-            '切分专家,单元测试用户'
-        )
-        self.add_users_by_admin(
-            [dict(email=r[0], name=r[2], password=r[1]) for r in [u.expert2]],
-            '切分审定员,单元测试用户'
+            [dict(email=r[0], name=r[2], password=r[1]) for r in [u.expert1, u.expert2]],
+            '切分专家,文字专家,数据管理员,单元测试用户'
         )
         self.add_users_by_admin(
             [dict(email=r[0], name=r[2], password=r[1]) for r in [u.expert3]],
