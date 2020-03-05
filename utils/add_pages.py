f3726fe8579db2742b7c0cd22f14f3608e5b5bc4
Zhang Yungui <rhcad@hotmail.com>
2020-03-05 09:32:19 +0800

    ch.column_id 改为 .name

--- utils/add_pages.py
+++ utils/add_pages.py
@@ -176,7 +176,7 @@ class AddPage(object):
 
 
 def main(db=None, db_name='tripitaka', uri='localhost', json_path='', img_path='img', txt_path='txt',
-         txt_field='ocr', kind='', source='', check_id=False, reorder=True, reset=True,
+         txt_field='ocr', kind='', source='', check_id=False, reorder=False, reset=True,
          use_local_img=False, update=False, check_only=False):
     """
     导入页面的主函数
