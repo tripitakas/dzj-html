1a153fd39775383f05fe534f23c3bfea16ce48f9
xiandu <lqs.xiandu@qq.com>
2020-03-07 16:30:19 +0800

    生成字表时分页处理，每页不超过50条

--- controller/data/gen_chars.py
+++ controller/data/gen_chars.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+import math
 from os import path
 
 BASE_DIR = path.dirname(path.dirname(path.dirname(__file__)))
@@ -34,43 +35,46 @@ def gen_chars(db=None, condition=None, page_names=None, username=None):
     elif isinstance(condition, str):
         condition = json.loads(condition)
 
-    pages = list(db.page.find(condition, {k: 1 for k in ['name', 'source', 'columns', 'chars']}))
-    # 查找、分类chars
-    chars, invalid_chars, invalid_pages = [], [], []
-    for p in pages:
-        try:
-            id2col = {col['column_id']: {k: col[k] for k in ['cid', 'x', 'y', 'w', 'h']} for col in p['columns']}
-            for c in p['chars']:
-                try:
-                    meta = dict(page_name=p['name'])
-                    meta['name'] = '%s_%s' % (p['name'], c['cid'])
-                    meta.update({k: c[k] for k in ['source', 'cid', 'char_id', 'ocr_txt', 'alternatives'] if c.get(k)})
-                    meta.update({k: int(c[k] * 1000) for k in ['cc', 'sc'] if c.get(k)})
-                    meta['txt'] = c.get('txt') or c.get('ocr_txt')
-                    meta['pos'] = dict(x=c['x'], y=c['y'], w=c['w'], h=c['h'])
-                    meta['column'] = id2col.get('b%sc%s' % (c['block_no'], c['column_no']))
-                    meta['uid'] = hp.align_code('%s_%s' % (p['name'], c['char_id'][1:].replace('c', '_')))
-                    chars.append(meta)
-                except KeyError:
-                    invalid_chars.append('%s_%s' % (p['name'], c['cid']))
-        except KeyError:
-            invalid_pages.append(p['name'])
-    # 更新已存在的chars
-    chars_dict = {c['name']: c for c in chars}
-    existed = list(db.char.find({'name': {'$in': [c['name'] for c in chars]}}))
-    for e in existed:
-        c = chars_dict.get(e['name'])
-        if is_changed(e, c):
-            db.char.update_one({'_id': e['_id']}, {'$set': {k: c.get(k) for k in ['char_id', 'uid', 'pos']}})
-    # 插入不存在的chars
-    existed_id = [c['name'] for c in existed]
-    un_existed = [c for c in chars if c['name'] not in existed_id]
-    if un_existed:
-        db.char.insert_many(un_existed, ordered=False)
-
-    log = dict(inserted_char=[c['name'] for c in un_existed], existed_char=[c['name'] for c in existed],
-               invalid_char=invalid_chars, invalid_pages=invalid_pages)
-    Bh.add_op_log(db, 'gen_chars', log, username)
+    once_size = 50
+    total_count = db.page.count_documents(condition)
+    fields = ['name', 'source', 'columns', 'chars']
+    for i in range(int(math.ceil(total_count / once_size))):
+        pages = list(db.page.find(condition, {k: 1 for k in fields}).skip(i * once_size).limit(once_size))
+        # 查找、分类chars
+        chars, invalid_chars, invalid_pages = [], [], []
+        for p in pages:
+            try:
+                id2col = {col['column_id']: {k: col[k] for k in ['cid', 'x', 'y', 'w', 'h']} for col in p['columns']}
+                for c in p['chars']:
+                    try:
+                        meta = dict(page_name=p['name'])
+                        meta['name'] = '%s_%s' % (p['name'], c['cid'])
+                        meta.update({k: c[k] for k in ['source', 'cid', 'char_id', 'ocr_txt', 'alternatives'] if c.get(k)})
+                        meta.update({k: int(c[k] * 1000) for k in ['cc', 'sc'] if c.get(k)})
+                        meta['txt'] = c.get('txt') or c.get('ocr_txt')
+                        meta['pos'] = dict(x=c['x'], y=c['y'], w=c['w'], h=c['h'])
+                        meta['column'] = id2col.get('b%sc%s' % (c['block_no'], c['column_no']))
+                        meta['uid'] = hp.align_code('%s_%s' % (p['name'], c['char_id'][1:].replace('c', '_')))
+                        chars.append(meta)
+                    except KeyError:
+                        invalid_chars.append('%s_%s' % (p['name'], c['cid']))
+            except KeyError:
+                invalid_pages.append(p['name'])
+        # 更新已存在的chars
+        chars_dict = {c['name']: c for c in chars}
+        existed = list(db.char.find({'name': {'$in': [c['name'] for c in chars]}}))
+        for e in existed:
+            c = chars_dict.get(e['name'])
+            if is_changed(e, c):
+                db.char.update_one({'_id': e['_id']}, {'$set': {k: c.get(k) for k in ['char_id', 'uid', 'pos']}})
+        # 插入不存在的chars
+        existed_id = [c['name'] for c in existed]
+        un_existed = [c for c in chars if c['name'] not in existed_id]
+        if un_existed:
+            db.char.insert_many(un_existed, ordered=False)
+        log = dict(inserted_char=[c['name'] for c in un_existed], existed_char=[c['name'] for c in existed],
+                   invalid_char=invalid_chars, invalid_pages=invalid_pages)
+        Bh.add_op_log(db, 'gen_chars', log, username)
 
 
 if __name__ == '__main__':
