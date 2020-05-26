983c04f7428a95d382cb780dd1c663fc2649ae13
Zhang Yungui <rhcad@hotmail.com>
2020-05-26 11:16:13 +0800

    再小一点

--- static/js/cut/cut.js
+++ static/js/cut/cut.js
@@ -545,10 +545,10 @@
           .attr({'stroke': 'transparent', fill: data.boxFill, cursor: 'crosshair'});
 
       state.readonly = p.readonly;
-      data.ratioInitial = $(data.holder).width() / p.width;
+      data.ratioInitial = ($(data.holder).width() - 10) / p.width;
       var h = data.scrollContainer ? data.scrollContainer.height() : $(data.holder).height();
       if (h && !p.widthFull) {
-        data.ratioInitial = Math.max(data.ratioInitial, (h - 6) / p.height);
+        data.ratioInitial = Math.max(data.ratioInitial, (h - 10) / p.height);
       }
       if (p.minRatio) {
         data.ratioInitial = Math.max(data.ratioInitial, p.minRatio);
