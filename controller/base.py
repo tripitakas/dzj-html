d9a333ec3db7915b2c20d3b5c843f96f2e7413a7
Zhang Yungui <rhcad@hotmail.com>
2020-06-08 22:30:58 +0800

    临时改为OSS图加当前时间，强制刷新字图

--- controller/base.py
+++ controller/base.py
@@ -331,8 +331,9 @@ class BaseHandler(CorsMixin, RequestHandler):
         # 从云盘获取图片
         my_cloud = self.get_config('web_img.my_cloud')
         if my_cloud:
-            ver = img_type == 'char' and self.db.char.find_one({'name': img_name})
-            ver = ver and ver.get('img_time') and '?v=' + ver.get('img_time') or ''
+            # ver = img_type == 'char' and self.db.char.find_one({'name': img_name})
+            # ver = ver and ver.get('img_time') and '?v=' + ver.get('img_time') or ''
+            ver = img_type == 'char' and get_date_time('?v=%m%d%H%M%S')
 
             auth = oss2.Auth(self.get_config('web_img.key_id'), self.get_config('web_img.key_secret'))
             bucket_name = re.sub(r'http[s]?://', '', my_cloud).split('.')[0]
