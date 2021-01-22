3aa2a9b4e06b851249d50ab8b92ccab4cf2f47b8
xiandu <lqs.xiandu@qq.com>
2021-01-22 08:47:43 +0800

    完善字框紧凑不能点击的bug

--- static/js/box/box.base.js
+++ static/js/box/box.base.js
@@ -342,13 +342,13 @@
 
   function findBoxByPoint(pt, boxType, func) {
     if (!pt || !pt.x) return;
-    if (status.curBox && isInRect(pt, status.curBox.elem, 8))
+    if (status.curBox && isInRect(pt, status.curBox.elem, 3))
       return status.curBox;
 
     let ret = null, dist = 1e5;
     data.boxes.forEach(function (box) {
       if (box.elem && (boxType === 'all' || box.boxType === boxType)
-          && isInRect(pt, box.elem, 3)) {
+          && isInRect(pt, box.elem, 5)) {
         if (func && !func(box)) return;
         for (let j = 0; j < 8; j++) {
           let d = getDistance(pt, getHandlePt(box.elem, j));
